3b84729ab4269544424c4c7f8e56af7be6781fa7
Maria Carvalho <miacsilvac@hotmail.com>
2024-06-04 20:55:54 +0100

    cors

--- config/index.js
+++ config/index.js
@@ -24,7 +24,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL, "https://marialuisasilvapsicologia.com"],
+      credentials: true,
     })
   );
 
